fec5185ae9d5aa500c5f69e131fca1aa2815f35c
coyotte508 <coyotte508@gmail.com>
2018-09-17 15:14:29 +0200

    v 1.1.0, fix rotation direction

--- src/hex.spec.ts
+++ src/hex.spec.ts
@@ -4,19 +4,18 @@ import Hex from "./hex";
 describe("Hex", () => {
     it ("should rotate left", () => {
         const hex = new Hex(-3, 2);
-
         hex.rotateLeft(2);
-        expect(hex).to.include({q: 1, r: -3});
+        expect(hex).to.include({q: 2, r: 1});
         hex.rotateLeft(4);
         expect(hex).to.include({q: -3, r: 2});
     });
 
-    it ("should rotate left with center", () => {
+    it ("should rotate right with center", () => {
         const hex = new Hex(-3, 2);
 
-        hex.rotateLeft(3, {q: -3, r: 2, s: 1});
+        hex.rotateRight(3, {q: -3, r: 2, s: 1});
         expect(hex).to.include({q: -3, r: 2});
-        hex.rotateLeft(1, {q: -2, r: 1, s: 1});
+        hex.rotateRight(1, {q: -2, r: 1, s: 1});
         expect(hex).to.include({q: -3, r: 1});
     });
 
